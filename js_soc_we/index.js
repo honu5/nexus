cabcfcc9e6211f6d8c6204aaf1941c40613821e3
Nama21yo <mataniayohanes@gmail.com>
2025-11-10 16:14:21 +0300

    feat: implement websocket in js

---
+++ js_soc_we/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// app.get("/", (req, res) => {
+//   //   res.send("Hello World from Websockets!");
+//   res.sendFile(__dirname + "/public/index.html");
+// });
+//   we can also use express.static
+app.use("/", express.static(__dirname + "/public"));
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("new_message", (data) => {
+    console.log("Message from client:", data.message);
+    // Broadcast the message to all connected clients
+    // io.emit("message_received", data.message); to the entire client to send certain thing from the server
+    // socket.broadcast.emit("message_received", data.message); to all other clients except the sender
+    socket.emit("message_received", data.message);
+  });
+
+  //   socket.on("from_client", () => {
+  //     console.log(`Receiving the event from the client`);
+  //   });
+
+  //   setInterval(() => {
+  //     let date = new Date();
+  //     let time =
+  //       date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  //     socket.emit("from_server", time);
+  //   }, 3000);
+});
+// app.listen
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+//
---
+++ js_soc_we/public/index.js
@@ -0,0 +1,33 @@
+console.log("Welcome to Websockets with Socket.io");
+let socket = io();
+
+// let btn = document.getElementById("btn");
+// btn.addEventListener("click", () => {
+//   socket.emit("from_client", "Hello from client");
+// });
+// // the server event is been taken and added
+// socket.on("from_server", () => {
+//   let div = document.getElementById("from_server");
+//   let p = document.createElement("p");
+//   p.textContent = "Message from server received";
+//   div.appendChild(p);
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded and parsed");
+  let input = document.getElementById("chat_box");
+  let msgList = document.getElementById("msg_list");
+  let sendButton = document.getElementById("send");
+
+  sendButton.addEventListener("click", () => {
+    let message = input.value;
+    socket.emit("new_message", { message: message });
+    input.value = "";
+  });
+
+  socket.on("message_received", (message) => {
+    let li = document.createElement("li");
+    li.textContent = message;
+    document.getElementById("msg_list").appendChild(li);
+  });
+});
